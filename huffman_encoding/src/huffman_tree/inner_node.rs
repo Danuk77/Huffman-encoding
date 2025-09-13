70eaa875b08792c2269953e3cf9f80e9dac5a5fb
Danuk <danuk77@live.co.uk>
2025-09-13 20:18:00 +0100

    First issue with using trait bounds to achieve polymorphism

--- huffman_encoding/src/huffman_tree/inner_node.rs
+++ huffman_encoding/src/huffman_tree/inner_node.rs
@@ -2,12 +2,12 @@ use crate::huffman_tree::HuffmanTreeNode;
 
 pub struct HuffmanTreeInnerNode<T: HuffmanTreeNode> {
     frequency: u32,
-    left_child: T,
-    right_child: T,
+    left_child: Box<T>,
+    right_child: Box<T>,
 }
 
 impl<T: HuffmanTreeNode> HuffmanTreeInnerNode<T> {
-    pub fn new(left_node: T, right_node: T) -> HuffmanTreeInnerNode<T> {
+    pub fn new(left_node: Box<T>, right_node: Box<T>) -> HuffmanTreeInnerNode<T> {
         let frequency = left_node.get_frequency() + right_node.get_frequency();
         HuffmanTreeInnerNode {
             frequency: frequency,
--- huffman_encoding/src/main.rs
+++ huffman_encoding/src/main.rs
@@ -6,9 +6,18 @@
 mod huffman_tree;
 mod min_heap;
 use huffman_tree::{HuffmanTreeNode, HuffmanTreeLeafNode, HuffmanTreeInnerNode};
-use min_heap::MinPriorityQueue;
+//use min_heap::MinPriorityQueue;
 
 fn main() {
-    let test_node = HuffmanTreeLeafNode::new(5, String::from("a"));
-    let priority_queue = MinPriorityQueue<impl HuffmanTreeNode>::new();
+    // Create the leaf nodes
+    let c = Box::new(HuffmanTreeLeafNode::new(12, String::from("c")));
+    let d = Box::new(HuffmanTreeLeafNode::new(13, String::from("d")));
+    let a = Box::new(HuffmanTreeLeafNode::new(5, String::from("a")));
+    let b = Box::new(HuffmanTreeLeafNode::new(9, String::from("b")));
+    let e = Box::new(HuffmanTreeLeafNode::new(16, String::from("e")));
+
+    // Create the inner nodes
+    let cd = Box::new(HuffmanTreeInnerNode::new(c, d));
+    let ab = Box::new(HuffmanTreeInnerNode::new(a, b));
+    let abe = Box::new(HuffmanTreeInnerNode::new(ab, e));
 }
