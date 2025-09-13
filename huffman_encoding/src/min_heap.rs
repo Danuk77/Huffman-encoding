fbecf22675a3ad6b8083effd40444fc8e90bc357
Danuk <danuk77@live.co.uk>
2025-09-14 00:13:57 +0100

    Added wip min heap for insertion

--- huffman_encoding/src/min_heap.rs
+++ huffman_encoding/src/min_heap.rs
@@ -1,29 +1,42 @@
-//use crate::HuffmanTreeNode;
-//
-//pub trait MinHeap<T: HuffmanTreeNode>{
-//    fn insert(&mut self, item_to_insert: T) -> ();
-//    fn peek(&mut self) -> Option<&T>;
-//}
-//
-//pub struct MinPriorityQueue<T: HuffmanTreeNode> {
-//    items: Vec<T>
-//}
-//
-//impl<T:HuffmanTreeNode> MinHeap<T> for MinPriorityQueue<T> {
-//    fn insert(&mut self, item_to_insert: T) -> () {
-//        self.items.push(item_to_insert);
-//        // TODO: 
-//        //  1. Append to the end of the vector
-//        //  2. Check upwards if the parent is less than the new added item
-//    }
-//
-//    fn peek(&mut self) -> Option<&T>{
-//        self.items.first()
-//    }
-//}
-//
-//impl<T:HuffmanTreeNode> MinPriorityQueue<T>{
-//    pub fn new() -> MinPriorityQueue<T>{
-//       MinPriorityQueue { items: Vec::new() } 
-//    }
-//}
+pub trait MinHeap<T: Ord> {
+    fn insert(&mut self, item_to_insert: Box<T>) -> ();
+    fn bubble_up(&mut self) -> ();
+    //fn bubble_down(&mu)
+    fn peek(&mut self) -> Option<&Box<T>>;
+}
+
+pub struct MinPriorityQueue<T: Ord> {
+    items: Vec<Box<T>>,
+}
+
+impl<T: Ord> MinHeap<T> for MinPriorityQueue<T> {
+    fn insert(&mut self, item_to_insert: Box<T>) {
+        self.items.push(item_to_insert);
+        if self.items.len() == 1{
+            return;
+        }
+        self.bubble_up();
+    }
+
+    fn bubble_up(&mut self) -> () {
+        let mut index_of_item_to_bubble_up = self.items.len() - 1;
+        let mut index_of_parent = (index_of_item_to_bubble_up - 1) / 2;
+
+        while self.items[index_of_parent] > self.items[index_of_item_to_bubble_up]{
+            self.items.swap(index_of_parent, index_of_item_to_bubble_up);
+
+            index_of_item_to_bubble_up = index_of_parent;
+            index_of_parent = (index_of_item_to_bubble_up - 1) / 2;
+        }
+    }
+
+    fn peek(&mut self) -> Option<&Box<T>> {
+        self.items.first()
+    }
+}
+
+impl<T: Ord> MinPriorityQueue<T> {
+    pub fn new() -> MinPriorityQueue<T> {
+        MinPriorityQueue { items: Vec::new() }
+    }
+}
